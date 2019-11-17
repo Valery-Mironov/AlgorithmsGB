fb98fe5e46e322d8e085cfc9cc103d19fb4f1fb0
Антон Алиев <Mr.Aliev.Anton@gmail.com>
2019-11-17 19:30:40 +0300

    Начал задание 1

---
+++ Lesson-4_Lib/Task1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4_Lib
+{
+    public class Task1
+    {
+        /// <summary>
+        /// Матрица значений количества путей
+        /// </summary>
+        int[,] matrix;
+
+        /// <summary>
+        /// Матрица поля разрешенных клеток
+        /// </summary>
+        int[,] desk;
+
+        /// <summary>
+        /// Ширина поля
+        /// </summary>
+        int width;
+
+        /// <summary>
+        /// Высота поля
+        /// </summary>
+        int height;
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="w">Ширина поля</param>
+        /// <param name="h">Высота поля</param>
+        public Task1(int w, int h)
+        {
+            this.width = w;
+            this.height = h;
+            this.matrix = new int[w, h];
+            this.desk = new int[w, h];
+
+        }
+
+        /// <summary>
+        /// Рассчитывает и заполняет матрицу ходов, с учетом препядствий
+        /// </summary>
+        /// <param name="matrix">Матрица количества ходов</param>
+        /// <param name="desk">Матрица поля с препядствиями</param>
+        public void MatrixCalc(ref int[,] matrix, int[,] desk)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                matrix[0, j] = 1;
+            }
+            for (int i = 1; i < width; i++)
+            {
+                matrix[i, 0] = 1;
+                for (int j = 1; j < height; j++)
+                {
+                    matrix[i, j] = matrix[i, j-1] + matrix[i-1, j];
+                }
+            }
+        }
+    }
+}
---
+++ matrix/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matrix
+{
+    class Program
+    {
+        static Point point;
+        static void Main(string[] args)
+        {
+            point.X=0;
+            point.Y=0;
+            int[,] matrix = new int[8, 8];
+
+            matrix[point.X, point.Y] = 1;
+            matrix[3, 4] = 0;
+            matrix[3, 5] = 0;
+            InitMatrix(matrix);
+
+
+            PrintMatrix(matrix);
+            Console.ReadKey();
+        }
+
+        static void PrintMatrix(int[,] matrix)
+        {
+            Console.Clear();
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Console.Write($"{matrix[i, j]}\t");
+                }
+                Console.WriteLine();
+
+            }
+        }
+
+        static void InitMatrix(int[,] matrix)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                matrix[0, j] = 1;
+            }
+            for (int i = 1; i < 8; i++)
+            {
+                matrix[i, 0] = 1;
+                for (int j = 1; j < 8; j++)
+                {
+                    matrix[i, j] = matrix[i, j-1] + matrix[i-1, j];
+                }
+            }
+        }
+    }
+}
